7bf5cfdb2ece4d9b2f45981cd403d3927178e2a2
yonas <yonijonahphineas0@gmail.com>
2023-08-17 06:57:46 +0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { HomeComponent } from './home/home.component';
+import { MainComponent } from './main/main.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import { WorkComponent } from './work/work.component';
+
+@NgModule({
+  declarations: [AppComponent, NavbarComponent, HomeComponent, MainComponent, AboutComponent, ContactComponent, ServicesComponent, WorkComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
