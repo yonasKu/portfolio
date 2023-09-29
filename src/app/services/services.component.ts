bf94be5e5224eace5edfb72967dce0baabe174ea
yonas <yonijonahphineas0@gmail.com>
2023-09-29 12:17:02 +0300

    seventh commit

--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -6,10 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.css'],
 })
 export class ServicesComponent {
-  isCollapsed: boolean[] = [true, true, true,true]; // Initialize with default collapse state for each item
+  isCollapsed: boolean[] = [true, true, true, true]; // Initialize with default collapse state for each item
 
   toggleCollapse(index: number): void {
+    console.log(this.isCollapsed);
     this.isCollapsed[index] = !this.isCollapsed[index];
-    
   }
 }
